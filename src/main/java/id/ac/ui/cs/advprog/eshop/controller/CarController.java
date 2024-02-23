9542c59d7598d3e306ba1f107a7bfb2ab3b56773
vinamyrnauli <vinasllgn@gmail.com>
2024-02-23 16:37:57 +0700

    fix lagi carcontrll untuk OCP

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -19,7 +19,7 @@ import id.ac.ui.cs.advprog.eshop.service.CarService;
 @RequestMapping("/car")
 public class CarController {
     @Autowired
-    private CarServiceImpl carService;
+    private CarService carService;
 
     @GetMapping("/createCar")
     public String createCarPage(Model model) {
