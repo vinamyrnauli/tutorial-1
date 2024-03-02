668fc6c1e9c986fc0eefe1a0e937f74370d7151f
vinamyrnauli <vinasllgn@gmail.com>
2024-03-02 21:18:15 +0700

    [REFACTOR] Apply OrderStatus enum to Order model tests

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,12 +1,10 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import java.util.Arrays;
 import java.util.List;
 
 import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Builder
 @Getter
